151410f713382a3afb4857c8c4f1d6bf451de119
maurolps <boroengenharia@gmail.com>
2023-10-26 20:51:59 -0300

    Update Display controller

--- displaycontroller.js
+++ displaycontroller.js
@@ -17,6 +17,7 @@ playerContainer.addEventListener('mouseout', (e) => {
   for (let i = 0; i < shipLength; i++) {
     const neighborId = 'p'+cell.dataset.x+(parseInt(cell.dataset.y)+i);
     const neighbor = document.getElementById(neighborId);
+    if (neighbor !== null)
     neighbor.style.backgroundColor = 'white';
   }
 
@@ -26,14 +27,20 @@ playerContainer.addEventListener('mouseout', (e) => {
 
 playerContainer.addEventListener('mouseover', (e) => {
   const cell = e.target;
+  const cellX = cell.dataset.x;
+  const cellY = cell.dataset.y;
   const shipLength = 5;
+  let cellColor = 'gray';
+
+  if (parseInt(cellY)+shipLength > 10) cellColor = 'red';
 
   for (let i = 0; i < shipLength; i++) {
-    const neighborId = 'p'+cell.dataset.x+(parseInt(cell.dataset.y)+parseInt(i));
+    const neighborId = 'p'+cellX+(parseInt(cellY)+parseInt(i));
     const neighbor = document.getElementById(neighborId);
-    neighbor.style.backgroundColor = 'gray';
+    if (neighbor !== null)
+    neighbor.style.backgroundColor = cellColor;
   }
-  cell.style.backgroundColor = 'gray';
+  cell.style.backgroundColor = cellColor;
 
 })
 
