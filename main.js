4d3774a9328c546022ef838307fc9428b98c3221
maurolps <boroengenharia@gmail.com>
2023-10-26 15:06:52 -0300

    Added Player factory]

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const Ship = (length) => {
   let hits = 0;
+
   const hit = () => {
     hits += 1;
     hits
   }
+
   const isSunk = () => hits >= length;
 
   return { hit, isSunk }
@@ -28,28 +30,50 @@ const Gameboard = () => {
 
   const receiveAttack = ( x, y) => {
     const target = board[x][y];
+    if (target === 1) return 'Invalid target';
     if (typeof(target) == 'object') {
       target.hit();
       if (target.isSunk()) return 'destroyed!';
       return 'hit!';
+    } else {
+      board[x][y] = 1;
+      return 'miss';
     }
-    return 'miss';
   }
 
 
   return { board, receiveAttack, placeShip }
 }
 
-const Player = () => {
+const Player = (name) => {
+ const playerName = name;
+ const board = Gameboard();
 
+ const attack = (player, x, y) => {
+  return player.receiveAttack( x, y);
+ }
+
+ const receiveAttack = (x, y) => {
+  return board.receiveAttack(x, y);
+ }
+
+ const placeShip = ( x, y , length) => {
+  board.placeShip (x, y, length);
+ }
+
+ placeShip( 3, 3, 3);
+
+ return { attack, receiveAttack, playerName}
 }
 
-const board = Gameboard();
-board.placeShip (3, 3, 3);
-board.receiveAttack(3, 3); // hit!
-board.receiveAttack(3, 3); // hit!
-board.receiveAttack(2, 3); // miss
-board.receiveAttack(3, 3); // destroyed!
+const player = Player('Player 1');
+const computer = Player('Computer');
+
+
+player.attack(computer, 3, 3); // hit!
+
+
+
 
 
 
